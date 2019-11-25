f3d32da642c914b970d23ae3ef8fd7512f5e210a
erkinkarincaoglu <none>
2019-11-25 13:07:12 +0200

    added multiple  payment in one go rejected by  %30 chance display colors

--- js/scripts.js
+++ js/scripts.js
@@ -22,27 +22,49 @@ $("#payment-button").click(function(e) {
         e.stopPropagation();
 
         payment = getElementById("cc-outlet")  + " payment " + getElementById("cc-payment");
-        //alert ( getElementById("cc-outlet")  + " payment " + getElementById("cc-payment"));
-        addNewItemToList(payment);
+        count = getChecked()
+        for (i =0;i<count;i++) {
+            acceptedProbability = Math.floor(Math.random() * 10);
+            rejected = (acceptedProbability>6) ? true : false;
+            if (rejected) {
+                addNewItemToList(payment + ' rejected' + ' - ' + new Date().toLocaleTimeString() , "red");
+            } else {
+                addNewItemToList(payment + ' approved' + ' - ' + new Date().toLocaleTimeString() , "green" );
+            }
 
 
-
+        }
     }
     
     form.addClass('was-validated');
 });
 
 
-function addNewItemToList(itemtext) {
+function addNewItemToList(itemtext, color) {
     var ul = document.getElementById("payments");
     var li = document.createElement("li");
     li.classList.add('list-group-item');
+    li.classList.add(color);
     li.appendChild(document.createTextNode(itemtext));
-    ul.appendChild(li);
+    //ul.appendChild(li);
+
+    ul.insertBefore(li, ul.childNodes[0]);
 }
 function getElementById(id){
 
     return document.getElementById(id).value;
 }
 
+function getChecked(){
+
+ if (document.getElementById("inlineRadio1").checked) {
+    return 1;
+ }
+ else if (document.getElementById("inlineRadio2").checked) {
+    return 5;
+ }
+ else if (document.getElementById("inlineRadio3").checked) {
+     return 10;
+  }
+}
 
